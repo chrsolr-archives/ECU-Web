2f3ea687af1c5cf78a9f73bf89a788533e73c761
Christian Soler <iamrelos@gmail.com>
2015-12-21 19:09:47 +0000

    Initial Node Setup

--- public/js/application.js
+++ public/js/application.js
@@ -15,18 +15,6 @@
                 $routeProvider.when('/', {
                     templateUrl: '/views/home.html',
                     caseInsensitiveMatch: true
-                }).when('/dashboard', {
-                    templateUrl: '/views/dashboard.html',
-                    caseInsensitiveMatch: true
-                }).when('/game-info/:permalink', {
-                    templateUrl: '/views/game-info.html',
-                    caseInsensitiveMatch: true
-                }).when('/browse', {
-                    templateUrl: '/views/browse.html',
-                    caseInsensitiveMatch: true
-                }).when('/no-access', {
-                    templateUrl: '/views/no-access.html',
-                    caseInsensitiveMatch: true
                 }).otherwise({
                     redirectTo: '/'
                 });
@@ -41,25 +29,25 @@
 
                 $rootScope.$on('$routeChangeStart', function(event, next, current) {
 
-                    if ($location.url() === '/' || $location.url() === '/no-access')
-                        NavigationServices.displayNavigationBar(false);
-                    else
-                        NavigationServices.displayNavigationBar(true);
+                    // if ($location.url() === '/' || $location.url() === '/no-access')
+                    //     NavigationServices.displayNavigationBar(false);
+                    // else
+                    //     NavigationServices.displayNavigationBar(true);
 
-                    if (Parse.User.current()) {
-                        var query = (new Parse.Query(Parse.Role));
-                        query.equalTo("name", "Admin");
-                        query.equalTo("users", Parse.User.current());
-                        query.first().then(function(adminRole) {
-                            if (!adminRole) {
-                                $location.path('/no-access');
-                            }
-                        });
-                    } else {
-                        $location.path('/');
-                    }
+                    // if (Parse.User.current()) {
+                    //     var query = (new Parse.Query(Parse.Role));
+                    //     query.equalTo("name", "Admin");
+                    //     query.equalTo("users", Parse.User.current());
+                    //     query.first().then(function(adminRole) {
+                    //         if (!adminRole) {
+                    //             $location.path('/no-access');
+                    //         }
+                    //     });
+                    // } else {
+                    //     $location.path('/');
+                    // }
 
-                    NavigationServices.closeSideBar()
+                    // NavigationServices.closeSideBar()
                 });
             }
         ]);
--- public/js/controllers/home-controller.js
+++ public/js/controllers/home-controller.js
@@ -3,19 +3,19 @@
 
     angular.module('controllers').controller('HomeController', ['$location', function($location) {
 
-        var vm = this;
+        // var vm = this;
 
-        if (Parse.User.current()) $location.path('/dashboard');
+        // if (Parse.User.current()) $location.path('/dashboard');
 
-        vm.submit = function(user) {
+        // vm.submit = function(user) {
 
-            Parse.User.logIn(user.email, user.password)
-                .then(function() {
-                        window.location = "/dashboard";
-                    },
-                    function(user, error) {
-                        console.log(error);
-                    });
-        };
+        //     Parse.User.logIn(user.email, user.password)
+        //         .then(function() {
+        //                 window.location = "/dashboard";
+        //             },
+        //             function(user, error) {
+        //                 console.log(error);
+        //             });
+        // };
     }]);
 })();
