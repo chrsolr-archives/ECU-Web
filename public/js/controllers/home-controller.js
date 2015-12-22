e1fb6c7cae3435da84870b86341a84a14f938eb7
Christian Soler <twenty40@gmail.com>
2015-12-22 17:45:13 -0600

    ...

--- public/js/controllers/home-controller.js
+++ public/js/controllers/home-controller.js
@@ -1,12 +1,18 @@
 (function () {
     'use strict';
 
-    angular.module('controllers').controller('HomeController', ['GlobalServices', function (GlobalServices) {
+    angular.module('controllers').controller('HomeController', ['GlobalServices', 'NewsServices',
+        function (GlobalServices, NewsServices) {
 
-        var vm = this;
+            var vm = this;
 
-        GlobalServices.getFeaturedVideo().then(function(data){
-            vm.featuredVideo = data;
-        });
-    }]);
+            GlobalServices.getFeaturedVideo().then(function (data) {
+                vm.featuredVideo = data;
+            });
+
+            NewsServices.getNews().then(function(data){
+                console.log(data);
+                vm.news = data;
+            });
+        }]);
 })();
---
+++ public/js/services/news-services.js
@@ -0,0 +1,31 @@
+(function () {
+    'use strict';
+
+    angular.module('services').factory('NewsServices', ['$q', function ($q) {
+
+        return {
+            getNews: function(){
+                var q = $q.defer();
+
+                var news = Parse.Object.extend("News");
+                var query = new Parse.Query(news);
+                query.find({
+                    success: function(objects) {
+                        var data = [];
+
+                        angular.forEach(objects, function (value, key) {
+                            data.push(value.toJSON());
+                        });
+
+                        q.resolve(data);
+                    },
+                    error: function(error) {
+                        q.reject("Error: " + error.code + " " + error.message);
+                    }
+                });
+
+                return q.promise;
+            }
+        }
+    }]);
+})();
