2cfd87d9f301400258c39b612344b9342ca2a94f
iamrelos <iamrelos@gmail.com>
2015-12-29 21:33:36 -0600

    Added meta data for sharing news

--- public/js/controllers/news-details-controller.js
+++ public/js/controllers/news-details-controller.js
@@ -8,6 +8,8 @@
 
             NewsServices.getNewsByPermalink($routeParams.permalink).then(function (data) {
                 vm.news = data;
+                //document.title = vm.news.title;
+                //angular.element(document.querySelector('meta[name=description]')).attr('content', vm.news.description);
             });
 
             vm.sanitizeHTML = function (html) {
