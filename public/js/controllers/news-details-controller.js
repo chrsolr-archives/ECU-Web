ddc18fb991018eee3580656d024b503319a84285
iamrelos <iamrelos@gmail.com>
2015-12-29 22:27:55 -0600

    Added meta data for sharing news

--- public/js/controllers/news-details-controller.js
+++ public/js/controllers/news-details-controller.js
@@ -8,12 +8,26 @@
 
             NewsServices.getNewsByPermalink($routeParams.permalink).then(function (data) {
                 vm.news = data;
-                //document.title = vm.news.title;
-                //angular.element(document.querySelector('meta[name=description]')).attr('content', vm.news.description);
+                setMetadata();
             });
 
             vm.sanitizeHTML = function (html) {
                 return $sce.trustAsHtml(html);
             };
+
+            function setMetadata(){
+                document.title = vm.news.title;
+                document.querySelector('meta[property="og:title"]').setAttribute('content', vm.news.title);
+                document.querySelector('meta[property="og:type"]').setAttribute('content', 'website');
+                document.querySelector('meta[property="og:url"]').setAttribute('content', 'http://ecu.herokuapp.com/news/details/' + vm.news.permalink);
+                document.querySelector('meta[property="og:image"]').setAttribute('content', vm.news.imageUrl);
+                document.querySelector('meta[property="og:description"]').setAttribute('content', vm.news.description);
+                //angular.element(document.querySelector('meta[name=description]')).attr('content', vm.news.description);
+                //angular.element(document.querySelector('meta[name=description]')).attr('content', vm.news.description);
+                //angular.element(document.querySelector('meta[name=description]')).attr('content', vm.news.description);
+                //angular.element(document.querySelector('meta[name=description]')).attr('content', vm.news.description);
+                //angular.element(document.querySelector('meta[name=description]')).attr('content', vm.news.description);
+                //angular.element(document.querySelector('meta[name=description]')).attr('content', vm.news.description);
+            }
         }]);
 })();
