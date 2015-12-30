f0a8a8e96183c9020ced966ce38a80e92f9ce781
iamrelos <iamrelos@gmail.com>
2015-12-29 22:37:59 -0600

    Added meta data for sharing news

--- public/js/controllers/news-details-controller.js
+++ public/js/controllers/news-details-controller.js
@@ -17,17 +17,18 @@
 
             function setMetadata(){
                 document.title = vm.news.title;
+
                 document.querySelector('meta[property="og:title"]').setAttribute('content', vm.news.title);
                 document.querySelector('meta[property="og:type"]').setAttribute('content', 'website');
                 document.querySelector('meta[property="og:url"]').setAttribute('content', 'http://ecu.herokuapp.com/news/details/' + vm.news.permalink);
                 document.querySelector('meta[property="og:image"]').setAttribute('content', vm.news.imageUrl);
                 document.querySelector('meta[property="og:description"]').setAttribute('content', vm.news.description);
-                //angular.element(document.querySelector('meta[name=description]')).attr('content', vm.news.description);
-                //angular.element(document.querySelector('meta[name=description]')).attr('content', vm.news.description);
-                //angular.element(document.querySelector('meta[name=description]')).attr('content', vm.news.description);
-                //angular.element(document.querySelector('meta[name=description]')).attr('content', vm.news.description);
-                //angular.element(document.querySelector('meta[name=description]')).attr('content', vm.news.description);
-                //angular.element(document.querySelector('meta[name=description]')).attr('content', vm.news.description);
+
+                document.querySelector('meta[property="twitter:title"]').setAttribute('content', vm.news.title);
+                document.querySelector('meta[property="twitter:card"]').setAttribute('content', vm.news.description);
+                document.querySelector('meta[property="twitter:url"]').setAttribute('content', 'http://ecu.herokuapp.com/news/details/' + vm.news.permalink);
+                document.querySelector('meta[property="twitter:image"]').setAttribute('content', vm.news.imageUrl);
+                document.querySelector('meta[property="twitter:description"]').setAttribute('content', vm.news.description);
             }
         }]);
 })();
