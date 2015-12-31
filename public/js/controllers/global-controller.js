c2861ddc048180c7fd0a3a9c71de547d0e34f0c2
iamrelos <iamrelos@gmail.com>
2015-12-31 17:24:33 -0600

    Added music player v0.3

--- public/js/controllers/global-controller.js
+++ public/js/controllers/global-controller.js
@@ -1,10 +1,18 @@
 (function () {
     'use strict';
 
-    angular.module('controllers').controller('GlobalController', ['$location', 'NavigationServices', '$uibModal', '$uibModalStack', 'GlobalServices', '$interval', '$window', '$document',
-        function ($location, NavigationServices, $uibModal, $uibModalStack, GlobalServices, $interval, $window, $document) {
+    angular.module('controllers').controller('GlobalController', ['$location', 'NavigationServices', '$uibModal', '$uibModalStack', 'GlobalServices', '$interval', '$window', '$document', 'SoundcloudServices',
+        function ($location, NavigationServices, $uibModal, $uibModalStack, GlobalServices, $interval, $window, $document, SoundcloudServices) {
 
             var global = this;
+            global.isAudioPlaying = false;
+            global.track = new Audio();
+            global.selectedTrackIndex = 0;
+
+            SoundcloudServices.getSoundcouldSongs().then(function(data){
+                global.songs = data.data;
+                global.selectedTrack = global.songs[global.selectedTrackIndex];
+            });
 
             global.contactUs = function () {
                 $uibModal.open({
@@ -20,6 +28,38 @@
                 GlobalServices.contactUs(global.contact).then(function () {
                     $uibModalStack.dismissAll();
                 })
+            };
+
+            global.playSong = function(index) {
+                if (index < 0) index = 0;
+
+                if (index >= global.songs.length) index = global.songs.length - 1;
+
+                global.selectedTrack = global.songs[index];
+                global.selectedTrackIndex = index;
+
+                if (global.isAudioPlaying){
+                    global.track.pause();
+                }
+
+                global.track = new Audio(global.selectedTrack.stream_url);
+                global.track.play();
+                global.isAudioPlaying = true;
+            };
+
+            global.stopSong = function() {
+                if (global.isAudioPlaying){
+                    global.track.pause();
+                    global.isAudioPlaying = false;
+                }
+            };
+
+            global.downloadSong = function(index){
+                window.open(global.songs[index].download_url);
+            };
+
+            global.openPlaylist = function(){
+                angular.element(document.querySelector('.soundcloud-player-wrapper')).toggleClass('soundcloud-player-toggle');
             };
 
             NavBar();
