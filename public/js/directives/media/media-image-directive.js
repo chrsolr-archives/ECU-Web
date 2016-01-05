d07d1cd4ad1b18419bdafc0b9c4d6c31a8b264ca
Christian Soler <iamrelos@gmail.com>
2016-01-05 21:41:46 +0000

    Working on directives

--- public/js/directives/media/media-image-directive.js
+++ public/js/directives/media/media-image-directive.js
@@ -6,22 +6,14 @@
         return {
             scope: {
                 destinationUrl: '@',
-                // title: '@',
-                // description: '@',
-                // imageUrl: '@',
                 media: '='
             },
             restrict: 'E',
             templateUrl: '/js/directives/media/media-image-directive.html',
             controller: function($scope){
-                console.log($scope);
-                                
                 var url = $scope.media.permalink || $scope.media.id;
                 
                 $scope.media.url = $scope.destinationUrl + url;
-                
-                console.log(url);
-
             }
         }
     }]);
