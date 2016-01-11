a379495f6f6fefd9f52ae3ef040c37b17a6ed16d
Christian Soler <twenty40@gmail.com>
2016-01-11 00:52:48 -0600

    Clean up global controller.

--- public/js/controllers/global-controller.js
+++ public/js/controllers/global-controller.js
@@ -5,13 +5,6 @@
         function ($scope, $location, NavigationServices, $uibModal, $uibModalStack, GlobalServices, $interval, $window, $document, SoundcloudServices) {
 
             var global = this;
-            global.selectedTrackIndex = 0;
-            global.scPlayer = new SoundCloud('500f3c5cdcf76cb1bcc8c35e97864840');
-
-            SoundcloudServices.getSoundcloudSongs().then(function (data) {
-                global.songs = data.data;
-                global.selectedTrack = global.songs[global.selectedTrackIndex];
-            });
 
             global.contactUs = function () {
                 $uibModal.open({
@@ -29,50 +22,6 @@
                 })
             };
 
-            global.playSong = function (index) {
-                if (index < 0) index = 0;
-
-                if (index >= global.songs.length) index = global.songs.length - 1;
-
-                global.selectedTrack = global.songs[index];
-                global.selectedTrackIndex = index;
-
-                global.scPlayer.resolve(global.selectedTrack.url, function (err, track) {
-
-                    global.scPlayer.play();
-
-                    $document.find('.soundcloud-player-wrapper').removeClass('soundcloud-player-show');
-                    $document.find('html, body').removeClass('no-scroll');
-                });
-            };
-
-            global.stopSong = function () {
-                if (global.scPlayer.playing) {
-                    global.scPlayer.pause();
-                }
-            };
-
-            global.scPlayer.on('ended', function (audio) {
-                global.playSong(global.selectedTrackIndex + 1);
-            });
-
-            global.downloadSong = function (index) {
-                window.open(global.songs[index].download_url);
-            };
-
-            global.togglePlaylist = function () {
-                var $playlist = $document.find('.soundcloud-player-wrapper');
-                var $body = $document.find('html, body');
-                var isOpen = $playlist.hasClass('soundcloud-player-show');
-                
-                if (isOpen) {
-                    $playlist.removeClass('soundcloud-player-show');
-                    $body.removeClass('no-scroll');
-                } else {
-                    $playlist.addClass('soundcloud-player-show');
-                    $body.addClass('no-scroll');
-                }
-            };
 
             $scope.$on('navigation:sidebar', function (event, data) {
                 global.isSideBarOpen = data;
@@ -84,7 +33,6 @@
                 var docWindow = angular.element($window);
                 var $nav = $document.find('.navigation-bar-wrapper');
                 var $sideBar = $document.find('.navigation-bar-sidebar-wrapper');
-                var $soundcloud = $document.find('.soundcloud-player-wrapper');
                 var lastPosition = 0;
                 var isScrolled = false;
                 var delta = 5;
@@ -104,11 +52,9 @@
                     if ((top > lastPosition) && (top > $nav.outerHeight())) {
                         $nav.addClass('navigation-bar-toggle');
                         $sideBar.removeClass('navigation-bar-sidebar-toggle');
-                        $soundcloud.addClass('soundcloud-player-hide');
                     } else {
                         if (top + docWindow.height() < angular.element($document).height()) {
                             $nav.removeClass('navigation-bar-toggle');
-                            $soundcloud.removeClass('soundcloud-player-hide');
                         }
                     }
 
