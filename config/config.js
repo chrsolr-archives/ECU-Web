f8b4095d3ca0b06161cc27e195cbc2bd805f783d
Christian Soler <twenty40@gmail.com>
2016-01-11 20:22:12 -0600

    Working on converting global controller to typescript.

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	server: {
     		port: process.env.PORT || 3000,
-    		env: process.env.NODE_ENV || 'dev';
+    		env: process.env.NODE_ENV || 'dev'
 	},
 	apis_keys: {
 		parse_app_key: process.env.parse_app_key || 'aWSciE1mXkreaNl9lC95hkW66oeZZRwdmIXCcD7Y',
